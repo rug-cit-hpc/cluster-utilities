6d929e6441092f6b952905b7cad673069360132c
Klemen Voncina <klemenvon@gmail.com>
2018-10-25 12:58:05 +0200

    implemented some basic error handling and added a QoL function for user group printing

--- Quota/quota.py
+++ Quota/quota.py
@@ -12,6 +12,13 @@ colors = {
 # Some evil global variables
 t_width = int(subprocess.check_output(['tput','cols'])) # checks terminal width, this should not change throughout program operation (because it should take less than 0.1 sec to run)
 
+# define custom exceptions here
+class PermissionError(Exception):
+	pass
+
+class DataError(Exception):
+	pass
+
 def percent_bar(percent):
 	# Makes a string that when printed shows a percentage bar
 	bar = "" # empty string
@@ -67,12 +74,18 @@ class Quota():
 	# Lets try this implementation
 	# Create an object that contains all quota information for one directory for one user
 	# Then we can print it or not
-	def __init__(self,group,directory):
+	def __init__(self,group,directory,debug_flag):
 		self.group = group
 		self.directory = directory
 		self.raw = subprocess.check_output(['lfs','quota','-g',str(self.group),str(self.directory),'-h'])
+		if(debug_flag):
+			print "{}Debug Raw{}".format(colors.get('red'),colors.get('end'))
+			print self.raw
+			print "{}End of Debug Raw{}".format(colors.get('red'),colors.get('end'))
+		self.check_raw() # check the raw for errors before further processing
 		# Initialize here just so we keep track of what's available in the object
 		self.ref = filter(None,self.raw.split('\n')[2].split(' '))
+		self.sanity_check()
 		self.dusage = strip_char(self.ref[1])
 		self.dalloc = strip_char(self.ref[2])
 		self.dlimit = strip_char(self.ref[3])
@@ -81,12 +94,26 @@ class Quota():
 		self.falloc = int(self.ref[6])
 		self.flimit = int(self.ref[7])
 		self.fgrace = self.ref[8]
+		if(debug_flag):
+			print "{}Debug object variables{}".format(colors.get('red'),colors.get('end'))
+			print "Disk Usage: {}	{}\nDisk Allocated: {}	{}\nDisk Limit: {}	{}\nFile Usage: {}	{}\nFile Allocated: {}	{}\nFile Limit: {}	{}\nDisk Grace: {}	{}\nFile Grace: {}	{}".format(self.dusage,type(self.dusage),self.dalloc,type(self.dalloc),self.dlimit,type(self.dlimit),self.fusage,type(self.fusage),self.falloc,type(self.falloc),self.flimit,type(self.flimit),self.dgrace,type(self.dgrace),self.fgrace,type(self.fgrace))
+			print "{}End Debug ojbect variables{}".format(colors.get('red'),colors.get('end'))
 		self.bar = percent_bar(float((float(self.dusage)/float(self.dalloc))*100))
 		# some more empty internal variables
 		self._warn_use = None
 		self._warn_files = None
 		# functions to call
 		self.gen_warnings()
+
+	def check_raw(self):
+		# checks for messages like "Permission denied."
+		if(self.raw == 'Permission denied.'):
+			raise PermissionError()
+
+	def sanity_check(self):
+		# check if there even is a quota for this user group otherwise raise exception
+		if(self.ref[2] in {None,'0'} or self.ref[6] in {None,'0'}):
+			raise DataError()
 
 	def gen_warnings(self):
 		# data warnings
@@ -123,6 +150,19 @@ class Quota():
 	def return_warnings(self):
 		return self._warn_use,self._warn_files
 
+def process_entity(group_id):
+	# if possible, extracts username from group id, otherwise returns the value back unchanged
+	gid = str(group_id) # we don't really need it as an int at any point
+	if(gid[:2] == '10'):
+		entity = 'user'
+		eid = 'p' + gid[2:]
+	elif(gid[:2] == '20'):
+		entity = 'user'
+		eid = 'f' + gid[2:]
+	else:
+		entity = 'user group'
+		eid = gid
+	return eid,entity
 
 def main():
 	groups = os.getgroups()
@@ -136,12 +176,23 @@ def main():
 	#mutex.add_argument('-u',type=str,help="Specify user. Only prints quota for specified user's own group.",const=groups)
 	#mutex.add_argument('-g',type=str,help="Specify group. Only prints quota for specified group.",const=groups,nargs='?')
 	parser.add_argument('-r',help="Only output quota info if user is close to limit or above quota",action="store_true")
+	parser.add_argument('-d',help="Produce debugging output. Please note this is for developer use and likely prints utter gibberish",action="store_true")
 	args = parser.parse_args()
 	for g in groups:
 		if(not args.r):
-			print "Quotas for user group {}{}{}".format(colors.get('bold'),g,colors.get('end'))
+			eid,entity = process_entity(g)
+			print "Quotas for {} {}{}{}".format(entity,colors.get('bold'),eid,colors.get('end'))
 		for d in directories:
-			quota = Quota(g,d)
+			try:
+				quota = Quota(g,d,args.d)
+			except PermissionError:
+				if not args.r:
+					print "You do not have permission list quota for {} {}{}{}".format(entity,colors.get('bold'),eid,colors.get('end'))
+				continue
+			except DataError:
+				if not args.r:
+					print "No data for {} {}{}{} in directory {}{}{}".format(entity,colors.get('bold'),eid,colors.get('end'),colors.get('bold'),d,colors.get('end'))
+				continue
 			if(args.r):
 				dwarn,fwarn = quota.return_warnings()
 				if(dwarn == 'red'):
