b52595a571322cac7ae94796127eeebb65294ab4
J.E.G.Ziemke@rug.nl <p288317@peregrine.hpc.rug.nl>
2019-07-16 16:58:38 +0200

    Changed highlight flag option. Output slurm file

--- clusterTest/peregrine_test_job.sh
+++ clusterTest/peregrine_test_job.sh
@@ -6,11 +6,6 @@
 #SBATCH --mem=1000
 #SBATCH --partition=short
 
-echo Peregrine test job starting...
-echo [Sleeping for 5 seconds]
-sleep 20
-echo Peregrine test job complete.
-
-#./peregrine_test_omp
-
 #export OMP_NUM_THREADS=7
+./peregrine_test_omp
+sleep 10
--- clusterTest/peregrine_test_omp.cc
+++ clusterTest/peregrine_test_omp.cc
@@ -2,14 +2,16 @@
 #include <omp.h>
 
 int main() {
-  
-  int total = omp_get_num_threads();
-  printf("Starting %d processes...", total);
-  
+
+  int total = 0;
+
   #pragma omp parallel num_threads(7) // Use 7 threads
   {
     printf("Hello World!\n");     
+    total = omp_get_num_threads();
   }
+
+  printf("TOTAL THREADS: %d\n\n", total);
 
   return 0;
 }
