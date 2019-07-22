14a6bb330b15d011aba1b9ed58ca13c0aa58ebfe
J.E.G.Ziemke@rug.nl <p288317@peregrine.hpc.rug.nl>
2019-07-22 15:56:58 +0200

    Changed number of cores test job uses. Renamed test_peregrine to peregrine_test

--- clusterTest/peregrine_test_job.sh
+++ clusterTest/peregrine_test_job.sh
@@ -2,10 +2,9 @@
 
 #SBATCH --job-name=test_job
 #SBATCH --time=00:01:00
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=7
 #SBATCH --mem=1000
 #SBATCH --partition=short
 
-#export OMP_NUM_THREADS=7
 ./peregrine_test_omp
 sleep 10
